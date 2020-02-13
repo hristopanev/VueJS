c0beb499d921eacd269c3f9d3d3603c93111c458
Hristo Panev <hristopanef@gmail.com>
2020-02-13 16:49:05 +0100

    VueJS

--- Introduction to VueJS/index.js
+++ Introduction to VueJS/index.js
@@ -1,6 +1,20 @@
 new Vue ({
     el: '#app',
     data: {
-        name: 'Ivan'
+        name: 'Ivan',
+        title: 'First Vue App',
+        hasTodos: true,
+        time: new Date(),
+        todos: [
+            'Do the dieshesh',
+            'By groceries',
+            'Make your bed',
+        ]
+    },
+
+    methods: {
+        updateTime() {
+            this.time = new Date();
+        }
     }
 });

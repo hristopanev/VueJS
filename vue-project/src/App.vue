d8750ebe07c248eff2b6d10a52d987c19c8b6e5c
Hristo Panev <hristopanef@gmail.com>
2020-02-18 12:04:26 +0100

    vue-project

---
+++ vue-project/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <HelloWorld></HelloWorld>
+  </div>
+</template>
+
+<script>
+
+
+
+import HelloWorld from './components/HelloWorld.vue';
+import Vue from 'vue';
+Vue.component('HelloWorld', HelloWorld);
+
+export default {
+  name: 'app',
+  data() {
+    return { title: 'from App'}
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

ea5a3bf4cb8a04656a61fbc90c3b03dce45e83a1
hristopanev <hristopanef@gmail.com>
2020-06-25 10:15:14 +0200

    components-demo

--- components-demo/src/components/HelloWorld.vue
+++ components-demo/src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
-  </div>
+    {{msg}}
+  </div>  
 </template>
 
 <script>
@@ -8,6 +9,11 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data: function() {
+    return {
+   
+    }
   }
 }
 </script>

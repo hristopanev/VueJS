edee8c6b05bdeb4ce39c23a1ad99ba475b5cf949
hristopanev <hristopanef@gmail.com>
2020-06-30 12:51:18 +0200

    cw

---
+++ components-workshop/src/components/Home.vue
@@ -0,0 +1,52 @@
+<template>
+  <app-content>
+    <template v-slot:nav>
+      <ul>
+        <li
+          @click="selectSubjectHandler(index)"
+          :class="{ active: index === selectSubjectIndex }"
+          v-for="(item, index) in subjects"
+          :key="index"
+        >
+          <a>{{ item.name }}</a>
+        </li>
+      </ul>
+    </template>
+    <template v-slot:info>
+        {{ selectSubject.content }}
+    </template>
+  </app-content>
+</template>
+
+<script>
+import AppContent from "./shared/Content.vue";
+export default {
+  components: {
+    AppContent
+  },
+  props: {
+    subjects: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      selectSubjectIndex: 0
+    };
+  },
+  methods: {
+    selectSubjectHandler(inx) {
+      this.selectSubjectIndex = inx;
+    }
+  },
+  computed: {
+      selectSubject() {
+          return this.subjects[this.selectSubjectIndex];
+      }
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ components-workshop/src/components/core/Navigation.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="navigation">
+    <ul>
+      <li v-for="(item, index) in navItems" v-bind:key="item.id">
+        <a 
+        :class="{ active: index === selectedIndex }" 
+        @click="navItemClickHandler(index)"
+        >{{item.name}}</a>
+      </li>
+    </ul>
+
+    <ul>
+      <li>
+        <a href="#">SUBJECT [+]</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    navItems: {
+      type: Array,
+      required: true
+    },
+    selectedIndex: {
+      type: Number,
+      required: true
+    }
+  },
+  methods: {
+    navItemClickHandler(index) {
+      this.$emit('navigate', index);
+    }
+  }
+};
+</script>
+
+<style scoped>
+li a {
+  cursor: pointer;
+}
+</style>
